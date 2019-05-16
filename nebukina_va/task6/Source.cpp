8a13e486ac99d16f0f0c28971f41b2c31bf1079a
NebukinaVA <43473978+NebukinaVA@users.noreply.github.com>
2019-05-16 10:32:43 +0300

    Update Source.cpp

--- nebukina_va/task6/Source.cpp
+++ nebukina_va/task6/Source.cpp
@@ -4,34 +4,37 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-class BullsCows
+class Methods
 {
+	int size;
 	int numb;
-	int bulls = 0;
-	int cows = 0;
+	int bull = 0;
+	int cow = 0;
 	vector <int> number;
 	vector <int> comp;
 	int attempt = 0;
 public:
-	BullsCows()
+	Methods() 
 	{
 	}
-	BullsCows(int n)
+	Methods(int n)
 	{
-		number.reserve(n);
-		comp.reserve(n);
+		size = n;
+		number.reserve(size);
+		comp.reserve(size);
 	}
-	~BullsCows()
+	void ChangeSize(int _size)
 	{
-		//~number;
-		//~comp;
+		size = _size;
 	}
 	void CreateNumber() // число компьютера
 	{
-		for (int i = 0; i < comp.size(); i++)
+		srand(time(NULL));
+		for (int i = 0; i < size; i++)
 		{
 			comp[i] = (rand() % 9 + 1);
 			for (int j = 0; j < i; j++)
@@ -47,9 +50,13 @@ public:
 	{
 		numb = n;
 	}
-	void SplitNumber() // разбить на разряды
+	vector <int> RetSplit()
 	{
-		int a;
+		return number;
+	}
+	void SplitNumber(int _numb) // разбить число игрока на разряды
+	{
+		int a = _numb;
 		for (int i = number.size() - 1; i >= 0; i--)
 		{
 			int d = pow(10, i);
@@ -59,7 +66,90 @@ public:
 	}
 	int CountBulls()
 	{
+		SplitNumber(numb);
+		for (int i = 0; i < size; i++)
+		{
+			if (number[i] == comp[i])
+				bull++;
+		}
+		return bull;
+	}
+	int CountCows()
+	{
+		SplitNumber(numb);
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if ((i != j) && (number[i] == comp[j]))
+					cow++;
+			}
+		}
+		return cow;
+	}
+	void Attempts()
+	{
+		attempt++;
+	}
+};
+
+class BullsAndCows: public Methods
+{
+	int size;
+//	Methods game = Methods(n);
+public:
+	BullsAndCows()
+	{
+		cout << "Welcome to the game 'Bulls and Cows'!\n";
+	}
+	void GetSize()
+	{
+		cout << "Enter the size of nubmer: ";
+		cin >> size;
+		ChangeSize(size);
+	}
+	void EnterNumber()
+	{
+		bool flag = true;
+		int numb;
+		while (flag)
+		{
+			cout << "Enter your number: ";
+			cin >> numb;
+			if ((numb < pow(10, (size - 1))) || (numb >= pow(10, (size + 1))))
+				cout << "Number doesn't sync with chosen size.\n";
+			SplitNumber(numb);
+			vector <int> points = RetSplit();
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if ((i = j) && (points[i] == points[j]))
+					{
+						flag = true;
+						cout << "Digits match.\n";
+					}
+					else flag = false;
+				}
+			}
+		}
+	    GetNumber(numb);
+		SplitNumber(numb);
+	}
+	void PrintResult()
+	{
+		int bull = CountBulls();
+		int cow = CountCows();
+		if (bull == size)
+		{
+			cout << "Congrats! The made-up number is:" << numb << endl;
+		}
+		cout << "Bulls: " << bull << "   Cows: " << cow << endl;
+	}
+	void GamingProcess()
+	{
+		GetSize();
+		EnterNumber();
 
 	}
-
 };
